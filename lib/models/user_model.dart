34f37851db3a590526750aec67ba49dc9cf8f1a3
sufiyan454 <muhammedshufiyan5@gmail.com>
2026-03-30 20:25:28 +0530

    Setup project structure with models, providers, and screens

---
+++ lib/models/user_model.dart
@@ -0,0 +1,13 @@
+class UserModel {
+  String name;
+  String phone;
+  int age;
+  String image;
+
+  UserModel({
+    required this.name,
+    required this.phone,
+    required this.age,
+    required this.image,
+  });
+}
