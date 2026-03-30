e095fa486758decaf4200c20397e9d27e4d32216
sufiyan454 <muhammedshufiyan5@gmail.com>
2026-03-30 21:21:10 +0530

    Implement user provider with add, search, sort, and lazy loading

--- lib/providers/user_provider.dart
+++ lib/providers/user_provider.dart
@@ -4,5 +4,54 @@ import 'package:flutter/material.dart';
 import 'package:totakx_project/models/user_model.dart';
 
 class UserProvider extends ChangeNotifier {
-  List<UserModel> users = [];
+   List<UserModel> _all = [];
+  List<UserModel> _display = [];
+
+  List<UserModel> get users => _display;
+
+  int page = 0;
+  int limit = 10;
+
+  void addUser(UserModel user) {
+    _all.add(user);
+    refresh();
+  }
+
+  void refresh() {
+    _display = [];
+    page = 0;
+    loadMore();
+  }
+
+  void loadMore() {
+    int start = page * limit;
+    if (start >= _all.length) return;
+
+    int end = start + limit;
+
+    _display.addAll(
+      _all.sublist(start, end > _all.length ? _all.length : end),
+    );
+
+    page++;
+    notifyListeners();
+  }
+
+  void search(String q) {
+    _display = _all.where((u) {
+      return u.name.toLowerCase().contains(q.toLowerCase()) ||
+          u.phone.contains(q);
+    }).toList();
+    notifyListeners();
+  }
+
+  void sortOlder() {
+    _all.sort((a, b) => b.age.compareTo(a.age));
+    refresh();
+  }
+
+  void sortYounger() {
+    _all.sort((a, b) => a.age.compareTo(b.age));
+    refresh();
+  }
 }
